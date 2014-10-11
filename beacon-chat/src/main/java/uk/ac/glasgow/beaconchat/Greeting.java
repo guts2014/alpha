a12a66494734badc0f1c83c3c9eaf3f344f9f944
Reinis Elksnis <reinis@elksnis.lv>
2014-10-11 09:57:20 +0100

    sql and post example

--- beacon-chat/src/main/java/uk/ac/glasgow/beaconchat/Greeting.java
+++ beacon-chat/src/main/java/uk/ac/glasgow/beaconchat/Greeting.java
@@ -1,19 +1,34 @@
 package uk.ac.glasgow.beaconchat;
 
 public class Greeting {
-	private final long id;
-    private final String content;
+	private long id;
+    private String content;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
-
-    public long getId() {
-        return id;
+    
+    public Greeting() {
+    	id = 0;
+    	content = "";
     }
 
-    public String getContent() {
-        return content;
-    }
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+    
+    
 }
--- beacon-chat/src/main/java/uk/ac/glasgow/beaconchat/GreetingController.java
+++ beacon-chat/src/main/java/uk/ac/glasgow/beaconchat/GreetingController.java
@@ -1,14 +1,22 @@
 package uk.ac.glasgow.beaconchat;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 public class GreetingController {
+	ObjectMapper mapper = new ObjectMapper(); 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
@@ -23,5 +31,10 @@ public class GreetingController {
 	public String home(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/greetback", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Greeting greetBack(@RequestBody Greeting greet) throws JsonParseException, JsonMappingException, IOException {
+		return greet;
 	}
 }
